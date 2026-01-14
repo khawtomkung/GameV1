b52c2003aa6d93d69e9f7ccabaca45cf04a326ca
khawtomkung <napad7874@gmail.com>
2026-01-14 12:11:48 +0700

    Fay added kb

--- game_0.py
+++ game_0.py
@@ -1,32 +1,75 @@
+# Real time game
+
+import time
+from pynput import keyboard
+
+
+class KBPoller:
+    def on_press(self, key):
+        try:
+            ch = key.char.lower()
+            self.pressed.add(ch)
+        except AttributeError:
+            pass
+
+    def on_release(self, key):
+        try:
+            ch = key.char.lower()
+            self.pressed.remove(ch)
+        except AttributeError:
+            pass
+
+    def __init__(self):
+        self.pressed = set()
+
+        listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        listener.start()
+
 running = True
 
 player_x = 10
 player_y = 10
 
+npc_x = 50
+npc_y = 50
+npc_speed_x = 1
+npc_speed_y = 2
+
 x_min = 0
-x_max = 100
+x_max = 60
 y_min = 0
-y_max = 100
+y_max = 60
+
+kb = KBPoller()
 
 def scan_keys():
-    inp = input("a/d/w/s to move, q to quit:")
-
-    return inp
+    return kb.pressed
 
 def render_state():
     print("player is at:", player_x, player_y)
+    print("npc is at:", npc_x, npc_y)
 
-def update_state(inp):
-    global player_x, player_y, running
-    if inp == "a":
+def update_state(keys):
+    global player_x, player_y, running, npc_x, npc_y, npc_speed_x, npc_speed_y
+
+    
+    npc_x += npc_speed_x
+    npc_y += npc_speed_y
+
+    if npc_x > x_max or npc_x < x_min:
+        npc_speed_x = -npc_speed_x
+    if npc_y > y_max or npc_y < y_min:
+        npc_speed_y = -npc_speed_y
+
+    if "a" in keys:
         player_x -= 1
-    elif inp == "d":
+    if "d" in keys:
         player_x += 1
-    elif inp == "w":
+    if "w" in keys:
         player_y -= 1
-    elif inp == "s":
+    if "s" in keys:
         player_y += 1
-    elif inp == "q":
+    if "q" in keys:
         running = False
 
     if player_x < x_min:
@@ -44,6 +87,8 @@ while running:
     # render game state (graphics)
 
     render_state()
-    inp = scan_keys()
+    keys = scan_keys()
 
-    update_state(inp)
+    update_state(keys)
+
+    time.sleep(0.1)
