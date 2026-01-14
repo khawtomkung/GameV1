94ac81e90ef4bbe22a39db0cb8a046ffa930a7f7
khawtomkung <napad7874@gmail.com>
2026-01-14 14:10:26 +0700

    test

--- graphics.py
+++
@@ -1,6 +0,0 @@
-class Graphics:
-    def render(self, player, npcs):
-        print("Player:", player.x, player.y)
-
-        for i, npc in enumerate(npcs):
-            print(f"NPC {i}:", npc.x, npc.y)
