7bc6baae77da30aca3eae315305769d7f3afedda
khawtomkung <napad7874@gmail.com>
2026-01-14 15:42:57 +0700

    update

--- graphics.py
+++ graphics.py
@@ -1,6 +1,5 @@
 class Graphics:
     def render(self, player, npcs):
-        print("\033c", end="")  # clear terminal
         print("Player:", player.x, player.y)
         for i, npc in enumerate(npcs):
             print(f"NPC {i}:", npc.x, npc.y)
